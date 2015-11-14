4c0d405cab0dbe3c6bf68aa83b712cc930245382
iD70my <l5_5@hotmail.com>
2015-11-14 11:39:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,4 +11,36 @@ jQuery(document).ready(function($){
 			video.get(0).play();
 		}
 	}
+	/*
+تسبيح	
+	*/
+var animationDelay = 2500;
+ 
+animateHeadline($('.cd-headline'));
+ 
+function animateHeadline($headlines) {
+	$headlines.each(function(){
+		var headline = $(this);
+		//trigger animation
+		setTimeout(function(){ hideWord( headline.find('.is-visible') ) }, animationDelay);
+		//other checks here ...
+	});
+}
+function hideWord($word) {
+	var nextWord = takeNext($word);
+	switchWord($word, nextWord);
+	setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+}
+ 
+function takeNext($word) {
+	return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+}
+ 
+function switchWord($oldWord, $newWord) {
+	$oldWord.removeClass('is-visible').addClass('is-hidden');
+	$newWord.removeClass('is-hidden').addClass('is-visible');
+}
+	/*
+تسبيح	
+	*/
 });
