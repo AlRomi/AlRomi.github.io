42259d21d97340dd12e4ed44a6cb89b3fa810d3c
iD70my <l5_5@hotmail.com>
2015-11-14 12:32:16 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-jQuery(document).ready(function($){
-	//this is used for the video effect only
-	if( $('.cd-bg-video-wrapper').length > 0 ) {
-		var videoWrapper = $('.cd-bg-video-wrapper'),
-			mq = window.getComputedStyle(document.querySelector('.cd-bg-video-wrapper'), '::after').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
-		if( mq == 'desktop' ) {
-			// we are not on a mobile device 
-			var	videoUrl = videoWrapper.data('video'),
-				video = $('<video loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
-			video.appendTo(videoWrapper);
-			video.get(0).play();
-		}
-	}
-});
+	/*
+تجربة	
+	*/
+var animationDelay = 2500;
+ 
+animateHeadline($('.cd-headline'));
+ 
+function animateHeadline($headlines) {
+	$headlines.each(function(){
+		var headline = $(this);
+		//trigger animation
+		setTimeout(function(){ hideWord( headline.find('.is-visible') ) }, animationDelay);
+		//other checks here ...
+	});
+}
+function hideWord($word) {
+	var nextWord = takeNext($word);
+	switchWord($word, nextWord);
+	setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+}
+ 
+function takeNext($word) {
+	return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+}
+ 
+function switchWord($oldWord, $newWord) {
+	$oldWord.removeClass('is-visible').addClass('is-hidden');
+	$newWord.removeClass('is-hidden').addClass('is-visible');
+}
+	/*
+تجربة	
+	*/
